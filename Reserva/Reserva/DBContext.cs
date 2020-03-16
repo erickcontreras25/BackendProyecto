b24d24a26d8894590e76db9b9637e149d0a7f935
Erick <erickgue@unitec.edu>
2020-03-16 00:37:58 -0600

    Api
    Api

---
+++ Reserva/Reserva/DBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reserva.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reserva
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions opciones) : base(opciones)
+        {
+
+        }
+
+        public virtual DbSet<Complejo> Complejo { get; set; }
+        public virtual DbSet<Cancha> Cancha { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelB)
+        {
+            new Complejo.Map(modelB.Entity<Complejo>());
+            new Cancha.Map(modelB.Entity<Cancha>());
+            base.OnModelCreating(modelB);
+        }
+
+
+    }
+}
